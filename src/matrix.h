7c0c9ff5c8f314520419cd079b377cbdc065b463
Askar Tsyganov <ashtsyganov@edu.hse.ru>
2024-01-11 06:00:59 +0300

    Fix merge conflict 2

--- src/matrix.h
+++ src/matrix.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <vector>
 
@@ -11,7 +12,7 @@ public:
     Matrix() : rows_(0), columns_(0) {
     }
 
-    Matrix(size_t n) : data_(n * n, 0), rows_(n), columns_(n) {
+    explicit Matrix(size_t n) : data_(n * n, 0), rows_(n), columns_(n) {
     }
 
     Matrix(size_t rows, size_t columns) : data_(rows * columns, 0), rows_(rows), columns_(columns) {
@@ -23,6 +24,7 @@ public:
           columns_(matrix.empty() ? 0 : matrix.back().size()) {
 
         for (size_t row = 0; row < rows_; ++row) {
+            assert(matrix[row].size() == columns_);
             for (size_t column = 0; column < columns_; ++column) {
                 (*this)(row, column) = matrix[row][column];
             }
@@ -38,24 +40,38 @@ public:
     }
 
     T& operator()(size_t row, size_t column) {
+        assert(row < rows_ && column < columns_);
+
         return data_[row * columns_ + column];
     }
 
     const T& operator()(size_t row, size_t column) const {
+        assert(row < rows_ && column < columns_);
+
         return data_[row * columns_ + column];
     }
 
-    Matrix<T>& operator+=(Matrix<T> other) {
-        for (size_t i = 0; i < rows_ * columns_; ++i) {
-            data_[i] += other[i];
+    Matrix<T>& operator+=(const Matrix<T>& other) {
+        assert(rows_ == other.Rows() && columns_ == other.Columns());
+
+        for (size_t row = 0; row < rows_; ++row) {
+            for (size_t column = 0; column < columns_; ++column) {
+                (*this)(row, column) += other(row, column);
+            }
         }
+
         return *this;
     }
 
-    Matrix<T>& operator-=(Matrix<T> other) {
-        for (size_t i = 0; i < rows_ * columns_; ++i) {
-            data_[i] -= other[i];
+    Matrix<T>& operator-=(const Matrix<T>& other) {
+        assert(rows_ == other.Rows() && columns_ == other.Columns());
+
+        for (size_t row = 0; row < rows_; ++row) {
+            for (size_t column = 0; column < columns_; ++column) {
+                (*this)(row, column) -= other(row, column);
+            }
         }
+
         return *this;
     }
 
@@ -67,6 +83,21 @@ public:
             }
         }
         return transpose;
+    }
+
+    // This function can be made more faster
+    Matrix<T> GetSubMatrix(std::pair<size_t, size_t> begin, std::pair<size_t, size_t> end) const {
+        assert(end.first >= begin.first && end.second >= begin.second);
+        Matrix<T> sub_matrix(end.first - begin.first + 1, end.second - begin.second + 1);
+
+        for (size_t row = begin.first; row < std::min(rows_, end.first + 1); ++row) {
+            for (size_t column = begin.second; column < std::min(columns_, end.second + 1);
+                 ++column) {
+                sub_matrix(row - begin.first, column - begin.second) = (*this)(row, column);
+            }
+        }
+
+        return sub_matrix;
     }
 
 private:
@@ -87,6 +118,16 @@ Matrix<T> operator-(const Matrix<T>& lhs, const Matrix<T>& rhs) {
     Matrix<T> tmp(lhs);
     tmp -= rhs;
     return tmp;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Matrix<T>& matrix) {
+    for (size_t row = 0; row < matrix.Rows(); ++row) {
+        for (size_t column = 0; column < matrix.Columns(); ++column) {
+            os << matrix(row, column) << (column == matrix.Columns() - 1 ? "\n" : " ");
+        }
+    }
+    return os;
 }
 
 }  // namespace s_fast
